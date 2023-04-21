365aacedc672945c1958d946a5280ae85c2d342d
MironAlikc <miron.alikc@gmail.com>
2023-04-21 10:24:18 +0300

    commit

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -3,9 +3,13 @@ import 'package:dio/dio.dart';
 import 'models/crypto_coin.dart';
 
 class CryptoCoinsRepository {
+  // метод который работает сетью
   Future<List<CryptoCoin>> getCoinsList() async {
+    //async метод нипонятно кода закончится
+    //await ожидаем когда этот метот совершится
     final response = await Dio().get(
-        'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,SOL,AID,CAG,DOV,&tsyms=USD');
+      'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,SOL,AID,CAG,DOV,&tsyms=USD',
+    );
 
     final data = response.data as Map<String, dynamic>;
     final dataRaw = data['RAW'] as Map<String, dynamic>;
@@ -23,4 +27,7 @@ class CryptoCoinsRepository {
     return ctyptoCoinsList;
   }
 }
- 
+
+// as Map<String, dynamic> - приводим прямым кастам к словарю String, dynamic
+// и дальше мы у славоря вызываем список entries
+//  entries - получаем значение все переведеные в знчение список 
